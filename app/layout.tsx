8d23217bfe3c4314e55b3653e8063ea4a0ad8e5c
sammitako <p.hansaem@hotmail.com>
2022-12-25 12:51:16 +0900

    Initial setup - Sanity Studio

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+import "../styles/globals.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
