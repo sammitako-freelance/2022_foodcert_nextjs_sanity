1ac97cb7d2e4bd74e4a2b5cc5e0874ceea8e35fb
sammitako <p.hansaem@hotmail.com>
2023-01-04 23:33:13 +0900

    News: modal logic

--- components/news/NoticeItem.tsx
+++ components/news/NoticeItem.tsx
@@ -119,101 +119,104 @@ const NoticeItem = ({ data }: Props) => {
                 </div>
               </div>
               {/* DOWNLOAD FILES */}
-              {data.file?.length + data.imageFile?.length > 0 && (
-                <div className="">
-                  <button
-                    className="py-2 px-4 rounded-md hover:bg-red-400 hover:text-custom-beige text-red-400 font-bold border-2 border-red-400 hover:drop-shadow-md"
-                    type="button"
-                    onClick={() => setShowModal(true)}
-                  >
-                    <div className="flex space-x-2 items-center">
-                      <ArrowDownTrayIcon className="w-5 h-5" />
+              {data.file &&
+                data.imageFile &&
+                data.file?.length + data.imageFile?.length > 0 && (
+                  <div className="">
+                    <button
+                      className="py-2 px-4 rounded-md hover:bg-red-400 hover:text-custom-beige text-red-400 font-bold border-2 border-red-400 hover:drop-shadow-md"
+                      type="button"
+                      onClick={() => setShowModal(true)}
+                    >
+                      <div className="flex space-x-2 items-center">
+                        <ArrowDownTrayIcon className="w-5 h-5" />
 
-                      <p>
-                        다운로드 {data.file.length + data.imageFile.length}개
-                        파일
-                      </p>
-                    </div>
-                  </button>
-                  {/* MODAL */}
-                  {showModal ? (
-                    <>
-                      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-5">
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                          {/*content*/}
-                          <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                            {/*header*/}
-                            <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                              <h3 className="text-3xl font-semibold">
-                                Download{" "}
-                                {data.file.length + data.imageFile.length}{" "}
-                                {data.file.length + data.imageFile.length === 1
-                                  ? "File"
-                                  : "Files"}
-                              </h3>
-                              <button
-                                className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                onClick={() => setShowModal(false)}
-                              >
-                                <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                  ×
-                                </span>
-                              </button>
-                            </div>
-                            {/*body*/}
-                            <div className="relative p-6 flex-auto divide-y-2 space-y-3">
-                              {data.file.map((f, idx) => (
-                                <div
-                                  key={f._id}
-                                  className=" text-slate-500 text-lg leading-relaxed pt-3"
+                        <p>
+                          다운로드 {data.file.length + data.imageFile.length}개
+                          파일
+                        </p>
+                      </div>
+                    </button>
+                    {/* MODAL */}
+                    {showModal ? (
+                      <>
+                        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none mx-5">
+                          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                            {/*content*/}
+                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                              {/*header*/}
+                              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                <h3 className="text-3xl font-semibold">
+                                  Download{" "}
+                                  {data.file.length + data.imageFile.length}{" "}
+                                  {data.file.length + data.imageFile.length ===
+                                  1
+                                    ? "File"
+                                    : "Files"}
+                                </h3>
+                                <button
+                                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                  onClick={() => setShowModal(false)}
                                 >
-                                  <a
-                                    href={`${data.fileURL[idx]}?dl=`}
-                                    className="hover:text-emerald-500"
+                                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                                    ×
+                                  </span>
+                                </button>
+                              </div>
+                              {/*body*/}
+                              <div className="relative p-6 flex-auto divide-y-2 space-y-3">
+                                {data.file.map((f, idx) => (
+                                  <div
+                                    key={f._id}
+                                    className=" text-slate-500 text-lg leading-relaxed pt-3"
                                   >
-                                    {f.fileName}
-                                  </a>
-                                </div>
-                              ))}
-                              {data.imageFile.map((i, idx) => (
-                                <div
-                                  key={i._id}
-                                  className=" text-slate-500 text-lg leading-relaxed pt-3"
+                                    <a
+                                      href={`${data.fileURL[idx]}?dl=`}
+                                      className="hover:text-emerald-500"
+                                    >
+                                      {f.fileName}
+                                    </a>
+                                  </div>
+                                ))}
+                                {data.imageFile.map((i, idx) => (
+                                  <div
+                                    key={i._id}
+                                    className=" text-slate-500 text-lg leading-relaxed pt-3"
+                                  >
+                                    <a
+                                      href={`${data.imageURL[idx]}?dl=`}
+                                      className="hover:text-emerald-500"
+                                    >
+                                      {i.fileName}
+                                    </a>
+                                  </div>
+                                ))}
+                              </div>
+                              {/*footer*/}
+                              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b mt-3">
+                                <button
+                                  className="text-red-400 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                  type="button"
+                                  onClick={() => setShowModal(false)}
                                 >
-                                  <a
-                                    href={`${data.imageURL[idx]}?dl=`}
-                                    className="hover:text-emerald-500"
-                                  >
-                                    {i.fileName}
-                                  </a>
-                                </div>
-                              ))}
-                            </div>
-                            {/*footer*/}
-                            <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b mt-3">
-                              <button
-                                className="text-red-400 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
-                                onClick={() => setShowModal(false)}
-                              >
-                                Close
-                              </button>
-                              <button
-                                className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                type="button"
-                                onClick={downloadAll}
-                              >
-                                Download All
-                              </button>
+                                  Close
+                                </button>
+                                <button
+                                  className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                  type="button"
+                                  onClick={downloadAll}
+                                >
+                                  Download All
+                                </button>
+                              </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-                    </>
-                  ) : null}
-                </div>
-              )}
+                        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                      </>
+                    ) : null}
+                  </div>
+                )}
             </div>
           </div>
           {/* CONTENT */}
