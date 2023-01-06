3acfb702b5f59961384a339555a74c585550c8ab
sammitako <p.hansaem@hotmail.com>
2023-01-06 17:52:37 +0900

    Bug Fixed: ScrollToTop position

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -14,7 +14,7 @@ const ScrollToTop = () => {
   return isScrolled ? (
     <button
       onClick={moveToTop}
-      className="!fixed right-[15px] bottom-16 xs:right-[29px] xs:bottom-24 z-40 opacity-1 transition-all duration-300"
+      className="!fixed right-[15px] bottom-[4.5rem] xs:right-[29px] xs:bottom-24 z-40 opacity-1 transition-all duration-300"
     >
       <ChevronDoubleUpIcon className="w-10 h-10 xs:w-12 xs:h-12 p-1 hover:shadow-md text-custom-black hover:bg-custom-black hover:text-custom-beige hover:rounded-full transition-all duration-300 ease-in-out" />
     </button>
